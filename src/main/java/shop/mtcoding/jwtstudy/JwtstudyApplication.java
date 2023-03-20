b676de291c1002d24f4e2a77e7829ad5d78097a0
YouHyunKim <yuhyunk7@gmail.com>
2023-03-20 14:11:45 +0900

    환경 셋팅

---
+++ src/main/java/shop/mtcoding/jwtstudy/JwtstudyApplication.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.jwtstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtstudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtstudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shop/mtcoding/jwtstudy/JwtstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.jwtstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtstudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
