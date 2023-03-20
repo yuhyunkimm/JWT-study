ea04be30cd17dedde9329b82b5ab45bcd074ff0a
YouHyunKim <yuhyunk7@gmail.com>
2023-03-20 14:59:35 +0900

    jwt 전자서명 코드 test

---
+++ src/test/java/shop/mtcoding/jwtstudy/example/JwtTest.java
@@ -0,0 +1,39 @@
+package shop.mtcoding.jwtstudy.example;
+
+import java.util.Date;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties.Jwt;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+
+public class JwtTest {
+
+    // 아래는 예시> 나만 알고 있는 시크릿 넘버를 알고 있어야 한다
+    // ABC(메타코딩) -> 1313AB
+    // ABC(시크) -> 5335KD
+
+    // 아래 토큰을 만들어서 준다 서버는 아래토큰 번호를 풀어보고 동일하면 인증완료가 된다
+    // 1313AB -> 토큰
+    @Test
+    public void createJwt_test() {
+        // give
+
+        // when
+        String jwt = JWT.create()
+                .withSubject("토근제목")
+                // 토큰 만료 기한(기한이 끝나면 재 로그인)
+                // currentTimeMillis 현재 시간
+                .withExpiresAt(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24 * 7))
+                // 아이디와 1(key)
+                .withClaim("id", 1)
+                .withClaim("role", "guest")
+                // secret키가 들어 가야한다 아래는 대칭키로 누구한테 공개 하지 않고
+                // 유저에 아이디와 권한을 가지고 있으면 된다
+                .sign(Algorithm.HMAC512("메타코딩"));
+
+        System.out.println(jwt);
+        // then
+    }
+}
